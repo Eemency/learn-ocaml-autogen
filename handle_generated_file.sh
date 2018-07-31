0d09ff455e5a477f97e034572e5181473ac9af6d
Loïc Peyrot <lo.peyrot@laposte.net>
2018-07-31 14:53:00 +0200

    That didn’t work

--- handle_generated_file.sh
+++ handle_generated_file.sh
@@ -8,20 +8,20 @@ echo "$TEMP"
 echo Removing trailing whitespaces
 sed -i.bak -E 's/[ \t]+$//' "$TEMP"
 echo Changing fill
-sed -i.bak -E 's/Replace this string by your implementation./CHANGED/; /CHANGED/G' "$TEMP"
+sed -i.bak -E 's/Replace this string by your implementation./CHANGED/' -E '/CHANGED/G' "$TEMP"
 #echo Adding line breaks
 #sed -i.bak -E '/CHANGED/G' "$TEMP"
 rm "$TEMP.bak"
 
 echo "$META"
-echo Spreading brackets
-sed -i.bak $'s/{"/{\\\n"/; s/}/\\\n}/' "$META"
-#echo Spreading right bracket
-#sed -i.bak $'s/}/\\\n}/' "$META"
+echo Spreading left bracket
+sed -i.bak $'s/{"/{\\\n"/' "$META"
+echo Spreading right bracket
+sed -i.bak $'s/}/\\\n}/' "$META"
 echo Breaking lines
 sed -i.bak $'s/,"/,\\\n"/g' "$META"
 echo Sticking back lists
-awk '/\[/{printf "%s",$0;next} 1' "$META" | tee "$META" #> /dev/null
+awk '/\[/{printf "%s",$0;next} 1' "$META" | tee "$META" > /dev/null
 echo Indenting
 sed -i.bak '/^[^{}]/{s/^/  /;}' "$META"
 rm "$META.bak"
